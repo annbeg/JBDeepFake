1799fe903381ec57d073a5fef405029546c72ec4
Aleksandr Butenko <alexbut1102@gmail.com>
2020-02-14 10:11:14 +0300

    Solution

--- test-task-master/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='test-task',
-    packages=find_packages(),
-    version='1.0.0',
-    description='Neuromation test task',
-    author='Username',
-)
